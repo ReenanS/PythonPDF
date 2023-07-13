011dd55e4e39eb188fc045c76f116fbbc1ae9f4b
ReenanS <reenan.s.s.rss@gmail.com>
2023-07-13 20:47:08 -0300

    fix: ajustando projeto

---
+++ GestaoDocumentos/ContractData.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDocumentos
+{
+    public class ContractData
+    {
+        public string NomeCliente { get; set; }
+        public string RgCliente { get; set; }
+        public string CpfCliente { get; set; }
+        public string EnderecoCliente { get; set; }
+        public string DescricaoConsorcio { get; set; }
+        public decimal ValorConsorcio { get; set; }
+        public int Parcelas { get; set; }
+        public decimal ValorParcela { get; set; }
+        public int DuracaoConsorcio { get; set; }
+        public string MesContemplacao { get; set; }
+        public string LocalData { get; set; }
+
+    }
+
+}
